8490f8e08e8a646a97e9b681f70b9fdbb4304624
Alex Williams <alex_williams@protonmail.com>
2020-01-07 10:09:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+    DOM()
+  } )
+  
+  
+function DOM() {
+    document.getElementById( "text" ).innerHTML = "This is really cool!";
+}

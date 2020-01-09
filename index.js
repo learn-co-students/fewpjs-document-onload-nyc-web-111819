5df550b294df757200fa6738e8038e866a5f6ddf
Miss-Cheese <nat.galligan@gmail.com>
2020-01-08 21:44:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateText()
+  });
+
+function updateText() {
+    const targetText = document.getElementById("text")
+    targetText.innerHTML = "This is really cool!"
+ }
